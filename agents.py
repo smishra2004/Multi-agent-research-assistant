a8b9c95f38c9acad74ad0e3370eeb5e26c5824bb
Shubham Mishra <shubham.smishra2004@gmail.com>
2026-06-14 14:22:14 +0530

    modified agents

--- agents.py
+++ agents.py
@@ -2,76 +2,72 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from tools import web_search , scrape_url 
+from tools import web_search_tool,scrape_url_tool
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#model setup 
-llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
+llm = ChatOpenAI(
+    model = "Qwen/Qwen2.5-7B-Instruct:together",
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.getenv("HF_TOKEN"),
+    temperature=0
+)
 
-
-#1st agent 
 def build_search_agent():
     return create_agent(
         model = llm,
-        tools= [web_search]
+        tools = [web_search_tool]
     )
-
-#2nd agent 
 
 def build_reader_agent():
     return create_agent(
         model = llm,
-        tools = [scrape_url]
+        tools = [scrape_url_tool]
     )
-
-
-#writer chain 
 
 writer_prompt = ChatPromptTemplate.from_messages([
     ("system", "You are an expert research writer. Write clear, structured and insightful reports."),
     ("human", """Write a detailed research report on the topic below.
 
-Topic: {topic}
+    Topic: {topic}
 
-Research Gathered:
-{research}
+    Research Gathered:
+    {research}
 
-Structure the report as:
-- Introduction
-- Key Findings (minimum 3 well-explained points)
-- Conclusion
-- Sources (list all URLs found in the research)
+    Structure the report as:
+    - Introduction
+    - Key Findings (minimum 3 well-explained points)
+    - Conclusion
+    - Sources (list all URLs found in the research)
 
-Be detailed, factual and professional."""),
+    Be detailed, factual and professional."""),    
 ])
 
 writer_chain = writer_prompt | llm | StrOutputParser()
-
-#critic_chain 
 
 critic_prompt = ChatPromptTemplate.from_messages([
      ("system", "You are a sharp and constructive research critic. Be honest and specific."),
     ("human", """Review the research report below and evaluate it strictly.
 
-Report:
-{report}
+    Report:
+    {report}
 
-Respond in this exact format:
+    Respond in this exact format:
 
-Score: X/10
+    Score: X/10
 
-Strengths:
-- ...
-- ...
+    Strengths:
+    - ...
+    - ...
 
-Areas to Improve:
-- ...
-- ...
+    Areas to Improve:
+    - ...
+    - ...
 
-One line verdict:
-..."""),
+    One line verdict:
+    ..."""),
 ])
 
 critic_chain = critic_prompt | llm | StrOutputParser()
